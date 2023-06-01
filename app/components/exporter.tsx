6ecb057a8cde7006a9b5dd16e78b657a6dff0fda
01121531 <129072969+01121531@users.noreply.github.com>
2023-06-01 14:45:12 +0800

    Add files via upload

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,9 +437,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>YDGPT Next Web</div>
+            <div className={styles["main-title"]}>ChatGPT Next Web</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/YDGPT-Next-Web
+              github.com/Yidadaa/ChatGPT-Next-Web
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
